6dc8e7a16c6c4f7e7110bc98eb3f0bed640a01c9
Nguyễn Đức Chiến <nobi@nobidev.com>
2021-11-26 05:59:21 +0000

    Build algorithm for matching

--- src/match-manager.spec.ts
+++ src/match-manager.spec.ts
@@ -1,5 +1,7 @@
 import { MatchManager } from './match-manager';
 import { EventEmitter } from 'events';
+import { randomUUID } from 'crypto';
+import { User } from './user';
 
 describe('MatchManager', () => {
   test('can initialize', () => {
@@ -7,5 +9,60 @@ describe('MatchManager', () => {
   });
   test('compatible with EventListener', () => {
     expect(new MatchManager()).toBeInstanceOf(EventEmitter);
+  });
+});
+
+describe('MatchManager algorithm', () => {
+  let manager: MatchManager;
+
+  beforeEach(() => {
+    manager = new MatchManager();
+  });
+
+  test('matching with 1 users', () => {
+    const u1: User = { id: randomUUID() };
+
+    manager.request(u1);
+    expect(manager.rooms).toHaveLength(1);
+    for (const room of manager.rooms) {
+      expect(room.members).toHaveLength(1);
+      expect(room.members).toStrictEqual([u1]);
+      expect(room.closed).toBe(false);
+      expect(room.isCompleted).toBe(false);
+    }
+  });
+
+  test('matching with 2 users', () => {
+    const u1: User = { id: randomUUID() };
+    const u2: User = { id: randomUUID() };
+
+    manager.request(u1);
+    manager.request(u2);
+    expect(manager.rooms).toHaveLength(1);
+    for (const room of manager.rooms) {
+      expect(room.members).toHaveLength(2);
+      expect(room.members).toStrictEqual([u1, u2]);
+      expect(room.closed).toBe(true);
+      expect(room.isCompleted).toBe(true);
+    }
+  });
+
+  test('matching with 3 users', () => {
+    const u1: User = { id: randomUUID() };
+    const u2: User = { id: randomUUID() };
+    const u3: User = { id: randomUUID() };
+
+    manager.request(u1);
+    manager.request(u2);
+    manager.request(u3);
+    expect(manager.rooms).toHaveLength(2);
+    for (const room of manager.rooms) {
+      const isFirstRoom = manager.rooms.indexOf(room) <= 0;
+
+      expect(room.members).toHaveLength(isFirstRoom ? 2 : 1);
+      expect(room.members).toStrictEqual(isFirstRoom ? [u1, u2] : [u3]);
+      expect(room.closed).toBe(isFirstRoom ? true : false);
+      expect(room.isCompleted).toBe(isFirstRoom ? true : false);
+    }
   });
 });
--- src/match-manager.ts
+++ src/match-manager.ts
@@ -1,5 +1,68 @@
+import { randomUUID } from 'crypto';
 import { EventEmitter } from 'events';
+import { Subject } from 'rxjs';
+import { User } from './user';
+
+export class Room extends Subject<User> {
+  public readonly roomId: string = randomUUID();
+
+  public readonly members: Array<User> = [];
+
+  public constructor() {
+    super();
+    this.subscribe({
+      next: this.onNext.bind(this),
+      complete: this.onComplete.bind(this),
+    })
+  }
+
+  protected onNext(user: User): void {
+    // Khi có 1 người dùng mới được thêm vào phòng
+    this.members.push(user);
+  }
+
+  protected onComplete(): void {
+    // Khi phòng được đánh dấu là đã đủ thành viên
+    this.closed = true;
+    for (const user of this.members) {
+      // Gửi tín hiệu matched kèm danh sách thành viên và id phòng
+      // user.sendSignal('Matched', this.id, this.members);
+    }
+  }
+
+  public get isCompleted(): boolean {
+    return this.members.length == 2;
+  }
+}
 
 export class MatchManager extends EventEmitter {
+  public readonly rooms: Array<Room> = [];
 
+  public request(user: User): Room {
+    // Lặp qua các room đang có
+    for (const room of this.rooms) {
+      // Nếu có 1 room nào đó còn nhận thêm thành viên
+      if (!room.closed) {
+        // Thêm thành viên vào room
+        room.next(user);
+
+        // Nếu đã đủ thành viên
+        if (room.isCompleted) {
+          // Đánh dấu là không nhận thêm thành viên
+          room.complete();
+
+          return room;
+        }
+      }
+    }
+
+    // Ngược lại, nếu không có room nào thoả mãn thì tạo mới
+    const room = new Room();
+    this.rooms.push(room);
+
+    // Thêm user như là thành viên đầu tiên
+    room.next(user);
+
+    return room;
+  }
 }
